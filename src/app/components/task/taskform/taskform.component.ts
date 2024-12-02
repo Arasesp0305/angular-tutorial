8dc6c28ddf9da7dbf2906ce2fd4324d268c6b381
Arasesp0305 <arasesp0305@iescarrillo.es>
2024-12-02 12:37:29 +0100

    intento arreglo deployment 2

--- src/app/components/task/taskform/taskform.component.ts
+++ src/app/components/task/taskform/taskform.component.ts
@@ -1,40 +1,59 @@
-import {CommonModule} from '@angular/common';
-import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
-import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {customValidator, customValidatorPriority} from './taskForm.validators';
-import {Task, TaskStatus} from '../../../models/task.models';
-import {ActivatedRoute, ParamMap} from '@angular/router';
-import {TaskService} from '../../../services/task.service';
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import {
+  customValidator,
+  customValidatorPriority,
+} from './taskform.validators';
+import { Task, TaskStatus } from '../../../models/task.models';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { TaskService } from '../../../services/task.service';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-taskform',
   standalone: true,
   imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './taskform.component.html',
-  styleUrl: './taskform.component.css'
+  styleUrl: './taskform.component.css',
 })
 export class TaskformComponent implements OnChanges, OnInit {
-
   @Input()
   taskToEdit: Task | null = null; // Tarea a editar (null si estamos añadiendo)
 
-  formTaskEdit: FormGroup
+  formTaskEdit: FormGroup;
 
-  constructor(private taskService: TaskService, private route: ActivatedRoute, formBuilder: FormBuilder) {
+  constructor(
+    private taskService: TaskService,
+    private route: ActivatedRoute,
+    formBuilder: FormBuilder
+  ) {
     this.formTaskEdit = formBuilder.group({
-      'name': ['', [Validators.required, Validators.maxLength(50)]],
-      'description': ['', [Validators.required, Validators.maxLength(255)]],
-      'priority': ['', [Validators.required, customValidatorPriority()]],
-      'expirationDate': ['', [Validators.required, customValidator()]],
-
-    })
+      name: ['', [Validators.required, Validators.maxLength(50)]],
+      description: ['', [Validators.required, Validators.maxLength(255)]],
+      priority: ['', [Validators.required, customValidatorPriority()]],
+      expirationDate: ['', [Validators.required, customValidator()]],
+    });
   }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params: ParamMap) => {
-      let id = params.get('id')
-      console.log(id)
-    })
+      let id = params.get('id');
+      console.log(id);
+    });
   }
 
   @Output() formSubmit = new EventEmitter<Task>();
@@ -70,7 +89,6 @@ export class TaskformComponent implements OnChanges, OnInit {
     }
   }
 
-
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['taskToEdit'] && changes['taskToEdit'].currentValue) {
       const task = changes['taskToEdit'].currentValue;
@@ -84,6 +102,4 @@ export class TaskformComponent implements OnChanges, OnInit {
       this.formTaskEdit.reset(); // Limpiar el formulario si no hay tarea para editar
     }
   }
-
-
 }
